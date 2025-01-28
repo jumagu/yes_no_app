e30798f7519cc177b22b6181a8e3d11c8f65a8a6
jumagu <jmgg.gutierrez@gmail.com>
2025-01-28 00:02:02 -0500

    refactor: reorganize imports and update chat layout with message input field

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:yes_no_app/presentation/widgets/chat/chat_sender_bubble.dart';
+
 import 'package:yes_no_app/presentation/widgets/chat/chat_reply_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/chat_sender_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -12,7 +14,6 @@ class ChatScreen extends StatelessWidget {
         centerTitle: false,
         title: const Text('Anonymous'),
         leading: const Padding(
-          // padding: EdgeInsets.all(8),
           padding: EdgeInsets.only(left: 8, top: 8, bottom: 8),
           child: CircleAvatar(
             backgroundImage: NetworkImage(
@@ -29,22 +30,26 @@ class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: [
-            Expanded(
-                child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return (index % 2 == 0)
+      child: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return (index % 2 == 0)
                     ? const ChatReplyBubble()
                     : const ChatSenderBubble();
-              },
-            )),
-            Text('Acá el input'),
-          ],
-        ),
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+            child: MessageFieldBox(),
+          ),
+        ],
       ),
     );
   }
